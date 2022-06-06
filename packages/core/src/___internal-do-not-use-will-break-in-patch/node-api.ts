150597c6f7bbccc54a211a295cb609d4cf605a2e
Josh Calder <josh.calder@thinkmill.com.au>
2022-06-06 16:02:08 +1000

    change createQueryAPI to set global

--- packages/core/src/___internal-do-not-use-will-break-in-patch/node-api.ts
+++ packages/core/src/___internal-do-not-use-will-break-in-patch/node-api.ts
@@ -1,10 +1,21 @@
-import { KeystoneConfig } from '../types';
+import { BaseListTypeInfo, KeystoneConfig } from '../types';
 import { createSystem } from '../lib/createSystem';
 import { initConfig } from '../lib/config/initConfig';
+import { KeystoneListsAPI } from './../types/context';
 
-export function createQueryAPI(config: KeystoneConfig, prismaClient: any) {
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var keystoneQueryAPI: KeystoneListsAPI<Record<string, BaseListTypeInfo>>;
+}
+
+function createKeystoneQueryAPI(config: KeystoneConfig, prismaClient: any) {
   const { getKeystone } = createSystem(initConfig(config));
   const keystone = getKeystone(prismaClient);
   keystone.connect();
   return keystone.createContext({ sudo: true }).query;
 }
+
+export const createQueryAPI = globalThis.keystoneQueryAPI || createKeystoneQueryAPI;
+
+if (process.env.NODE_ENV !== 'production') globalThis.keystoneQueryAPI = createQueryAPI;
